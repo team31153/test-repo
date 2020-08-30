8db98e93a97e6f445a42e598ea357686a7184b71
Felixxxxxxxxxxxxxx <fantastic_falcon@icloud.com>
2020-08-30 11:31:51 -0400

    turtlebars.py

---
+++ Felix/Felix/Chapter5HW/turtlebars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pybricks-micropython
+import turtle
+def draw_bar(t, height):
+    """ Get turtle t to draw one bar, of height. """
+    t.begin_fill()           # Added this line
+    t.left(90)
+    t.forward(height)
+    t.write("  "+ str(height))
+    t.right(90)
+    t.forward(40)
+    t.right(90)
+    t.forward(height)
+    t.left(90)
+    t.end_fill()
+    tess.penup()             # Added this line
+    t.forward(10)
+    tess.pendown()
+
+wn = turtle.Screen()         # Set up the window and its attributes
+wn.bgcolor("lightgreen")
+
+tess = turtle.Turtle()       # Create tess and set some attributes
+tess.color("blue", "red")
+tess.pensize(3)
+
+xs = [48,117,200,240,160,260,220]
+
+for a in xs:
+    draw_bar(tess, a)
+
+wn.mainloop()
